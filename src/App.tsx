0b1c35632b46a0a553f6daed7108c981a985d8e1
Juan Villanueva <jvrvillanueva.06@gmail.com>
2025-11-18 12:56:13 -0500

    sponsorscard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PruebaPonentes from "./pages/PruebaPonentes";
 import PruebaDetallePonentes from "./pages/PruebaDetallePonentes";
+import PruebaModulos from "./pages/PruebaSponsor";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/ponentes" element={<PruebaPonentes />} />
       <Route path="/ponentes/:id" element={<PruebaDetallePonentes />} />
+      <Route path="/sponsors" element={<PruebaModulos />} />
     </Routes>
   );
 }
